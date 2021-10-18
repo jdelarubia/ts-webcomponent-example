ef30910d8509e03ab5086f82b9b3645e59d39d0a
jdelarubia <jdelarubia@gmail.com>
2021-10-18 23:07:27 +0200

    Main app

--- src/ts/main.ts
+++ src/ts/main.ts
@@ -1,1 +1,7 @@
-console.log("Hi from TS!");
+import { AgeToCode } from "./AgeToCode";
+import { MotivationPhrase } from "./MotivationPhrase";
+
+window.customElements.define("age-to-code", AgeToCode, { extends: "input" });
+window.customElements.define("motivation-phrase", MotivationPhrase, {
+    extends: "p",
+});
