d2f54a63ab2d94165b327702facb1ccfeddf996a
jdelarubia <jdelarubia@gmail.com>
2024-03-07 21:51:01 +0100

    component dist

--- src/wc-word-count/src/wc-word-count.js
+++ src/wc-word-count/src/wc-word-count.js
@@ -1,34 +1,49 @@
 "use strict";
 /**
  * wc-word-count.ts
- * Simple Word counter element.
- * Creates an editable article with its number of words below.
+ *
+ * Creates an editable textarea which shows the number of words on the bottom right corner.
  * The position and look of element can be set via CSS.
  */
 class WordCount extends HTMLElement {
     template;
+    initText;
     refreshRate;
     // defaults
-    static REFRESH = 1500;
-    constructor(refreshRate) {
+    static REFRESH = 2500;
+    static WIDTH = '20em';
+    static HEIGHT = '10em';
+    static INITTEXT = 'Enter some text here...';
+    static CSSFILE = './src/wc-word-count.css';
+    constructor(attrs = {}) {
         super();
-        this.refreshRate = refreshRate || WordCount.REFRESH;
+        this.refreshRate = attrs.refreshRate || WordCount.REFRESH;
+        this.initText = this.textContent || attrs?.initText || WordCount.INITTEXT;
+        this.textContent = '';
         this.template = document.createElement('template');
         this.template.innerHTML = this._render();
     }
     connectedCallback() {
         this.appendChild(this.template.content.cloneNode(true));
-        const editableContent = this.querySelector('#word-counter-container');
-        const wordCount = this.querySelector('#word-counter');
+        const editableContent = this.querySelector('textarea');
+        const wordCount = this.querySelector('div#word-counter');
         setInterval(() => {
             const content = editableContent.textContent || '';
             wordCount.textContent = this.countWords(content).toString();
         }, this.refreshRate);
     }
+    _getElementDimensions() {
+        const width = this.getAttribute('width');
+        const height = this.getAttribute('height');
+        return [height ? height : WordCount.HEIGHT, width ? width : WordCount.WIDTH];
+    }
     _render() {
-        const css = `<style> @import "./src/wc-word-count.css"; </style>`;
-        const html = `<article contenteditable="" id="word-counter-container">Edit your text here...</article>
-    <div class="counter" id="word-counter"></div>`;
+        const [height, width] = this._getElementDimensions();
+        const css = `<style> @import ${WordCount.CSSFILE}; </style>`;
+        const html = `<div class="word-counter" style="width: ${width}; height: ${height}">
+    <textarea contenteditable>${this.initText}</textarea>
+    <div id="word-counter"></div>
+    </div>`;
         return css + html;
     }
     /**
@@ -43,3 +58,10 @@ class WordCount extends HTMLElement {
     }
 } //. WordCount
 window.customElements.define('wc-word-count', WordCount);
+// Create and add a WordCount component programmatically
+const secondHalf = document.querySelector('#second-half');
+secondHalf?.appendChild(new WordCount({
+    width: '10em',
+    height: '5em',
+    initText: 'Lorem ipsum dolor sit amet consectetur adipisicing elit',
+}));
