ddda4f1c4794a7b620027e4c3dddad6861cdd652
jdelarubia <jdelarubia@gmail.com>
2024-03-10 22:14:16 +0100

    dist

--- src/wc-word-count/src/wc-word-count.js
+++ src/wc-word-count/src/wc-word-count.js
@@ -12,19 +12,19 @@
  */
 class WordCount extends HTMLElement {
     template;
-    initText;
+    placeholder;
     refreshRate;
     // defaults
     static REFRESH = 2500;
     static WIDTH = '20em';
     static HEIGHT = '10em';
-    static RESIZE = true;
-    static INITTEXT = 'Enter some text here...';
+    static RESIZE = 'both';
+    static PLACEHOLDER = 'Enter some text here...';
     static CSSFILE = './src/wc-word-count.css';
     constructor(attrs = {}) {
         super();
-        this.refreshRate = this._getRefreshRate() || attrs?.refreshRate || WordCount.REFRESH;
-        this.initText = this.textContent || attrs?.initText || WordCount.INITTEXT;
+        this.refreshRate = Number(attrs?.refreshRate || this._getAttributeOrDefault('refreshrate', WordCount.REFRESH));
+        this.placeholder = attrs?.placeholder || this.textContent || WordCount.PLACEHOLDER;
         this.textContent = '';
         this.template = document.createElement('template');
         this.template.innerHTML = this._render();
@@ -35,38 +35,40 @@ class WordCount extends HTMLElement {
         const wordCount = this.querySelector('div#word-counter');
         setInterval(() => {
             const content = editableContent.value || '';
-            wordCount.textContent = this.countWords(content).toString();
+            wordCount.textContent = this._countWords(content).toString();
         }, this.refreshRate);
     }
     _getResize() {
         const resize = this.getAttribute('resize')?.toLowerCase();
-        return resize === 'false' ? 'none' : 'both';
+        return resize === 'false' ? 'none' : WordCount.RESIZE;
     }
-    _getRefreshRate() {
-        const refreshRate = this.getAttribute('refreshrate');
-        return parseInt(refreshRate) || null;
-    }
-    _getElementDimensions() {
-        const width = this.getAttribute('width');
-        const height = this.getAttribute('height');
-        return [height ? height : WordCount.HEIGHT, width ? width : WordCount.WIDTH];
+    /**
+     * Generic method to retrieve the value of an attribute or, if not present, a default value.
+     * @param id string
+     * @param defaultValue string|number
+     * @returns string|number
+     */
+    _getAttributeOrDefault(id, defaultValue) {
+        const attr = this.getAttribute(id)?.toLowerCase();
+        return attr || defaultValue;
     }
     _render() {
-        const [height, width] = this._getElementDimensions();
+        const width = this._getAttributeOrDefault('width', WordCount.WIDTH);
+        const height = this._getAttributeOrDefault('height', WordCount.HEIGHT);
         const resize = this._getResize();
         const css = `<style> @import ${WordCount.CSSFILE}; </style>`;
         const html = `<div class="word-counter" style="width: ${width}; height: ${height}">
-    <textarea id="word-counter-container" placeholder="${this.initText}" style="resize:${resize}"></textarea>
+    <textarea id="word-counter-container" placeholder="${this.placeholder}" style="resize:${resize}"></textarea>
     <div id="word-counter"></div>
     </div>`;
         return css + html;
     }
     /**
      * Given some text, return the number of words in that text.
-     * @param textContent
+     * @param textContent string
      * @return number
      */
-    countWords(textContent) {
+    _countWords(textContent) {
         if (textContent == '' || textContent.length === 0 || !textContent)
             return 0;
         return textContent.trim().toLocaleLowerCase().split(' ').length;
