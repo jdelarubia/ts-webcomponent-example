fe0c6df45a2e4da3c23299b380476cbb1c31497c
jdelarubia <jdelarubia@gmail.com>
2024-03-12 21:59:30 +0100

    improve component docs

--- src/wc-instagram-btn/src/wc-instagram-btn.ts
+++ src/wc-instagram-btn/src/wc-instagram-btn.ts
@@ -1,6 +1,13 @@
 /**
  * wc-instagram-btn.ts
  * <instagram-btn> web component.
+ * It reads the following parameters or chooses some defaults:
+ * - tag (destination element ID)
+ * - color (primary color)
+ * - background (component background color)
+ * - opacity (float percent)
+ * - strokeWidth (float percent to denote the stroke thickness)
+ * - position ("top", "bottom" & "left", "right")
  *
  * define component in main:
  * window.customElements.define('instagram-btn', InstagramBtn)
